69c2b3ecd9a02cd504bd323e075744160e064bda
Георгий Крюков <gkryukov-ki21@stud.sfu-kras.ru>
2024-12-19 21:08:00 +0700

    bug fix

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 
-double my_pow(double n, unsigned int m) {
+double my_pow(double n, int m) {
     if (m > 1)
         return my_pow(n*n, m-1);
     if (m == 1)
         return n;
     if (m == 0)
         return 1;
+    if (m < 0) {
+        double res = 1;
+        for (int i = 0; i > m; i--) {
+            res = 1 / n;
+        }
+        return res;
+    }
 }
