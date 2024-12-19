59297d72aff6619b4536c4d60232bfe805832d7d
Георгий Крюков <gkryukov-ki21@stud.sfu-kras.ru>
2024-12-19 21:21:17 +0700

    asdf

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 
 double my_pow_bug_fix(double n, int m) {
-    if (m > 1)
-        return my_pow(n*n, m-1);
-    if (m == 1)
-        return n;
-    if (m == 0)
-        return 1;
+    double res = 1;
+    if (m > 0) {
+        while (m) {
+            if (m % 2 == 0) {
+                m /= 2;
+                n *= n;
+            }
+            else {
+                n--;
+                res *= n;
+            }
+        }
+        return res;
+    }
     if (m < 0) {
         double res = 1;
         for (int i = 0; i > m; i--) {
